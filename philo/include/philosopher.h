7756dfca05675ddac0e856819f0ce094bc106fd8
ThzEdouard <edouard.flaquet@gmail.com>
2022-09-21 17:11:06 +0200

    structt

--- philo/include/philosopher.h
+++ philo/include/philosopher.h
@@ -6,7 +6,7 @@
 /*   By: eflaquet <eflaquet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 14:11:15 by eflaquet          #+#    #+#             */
-/*   Updated: 2022/09/21 14:28:18 by eflaquet         ###   ########.fr       */
+/*   Updated: 2022/09/21 16:57:50 by eflaquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,26 @@
 
 # include <unistd.h>
 # include <stdio.h>
+# include <stdlib.h>
 # include <pthread.h>
+
+# define LOCK 1
+# define UNLOCK 0
 
 typedef struct s_fork
 {
+	int				pid;
+	int				loking;
 	pthread_mutex_t	mutex_fork;
 }				t_fork;
 
 typedef struct s_philosopher
 {
 	int			pid;
+	int			die;
+	pthread_t		thread_philo;
+	pthread_mutex_t	mutex_philo;
+	t_fork		fork;
 }				t_philosopher;
 
 typedef struct s_arg
@@ -34,6 +44,13 @@ typedef struct s_arg
 	int			time_to_eat;
 	int			time_to_sleep;
 	int			number_of_times_each_philosopher_must_eat;
+	t_philosopher	*philo;
 }				t_arg;
+
+
+// utils fontion
+unsigned int	u_atoi(const char *arg);
+// funtion for struct
+int		creating_philo(t_arg *arg, char	**argv);
 
 #endif
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,13 +6,52 @@
 /*   By: eflaquet <eflaquet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 14:12:35 by eflaquet          #+#    #+#             */
-/*   Updated: 2022/09/21 14:25:26 by eflaquet         ###   ########.fr       */
+/*   Updated: 2022/09/21 17:10:52 by eflaquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philosopher.h"
 
+static int	ft_isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	return (0);
+}
+
+static int	verif_arg(char **argv, int i)
+{
+	unsigned int	x;
+
+	x = 0;
+	while (argv[i])
+	{
+		while (argv[i][x])
+		{
+			if (ft_isdigit(argv[i][x]))
+				return (0);
+			x++;
+		}
+		i++;
+	}
+	return (1);
+}
+
 int	main(int argc, char **argv)
 {
+	t_arg	arg;
 
+	if ((argc < 4 &&  argc <=5) && verif_arg(argv, 1))
+	{
+		arg.number_of_philosophers = u_atoi(argv[1]);
+		arg.time_to_die = u_atoi(argv[2]);
+		arg.time_to_eat = u_atoi(argv[3]);
+		arg.time_to_sleep = u_atoi(argv[4]);
+		if (argc == 5)
+			arg.number_of_times_each_philosopher_must_eat = u_atoi(argv[5]);
+	}
+	else
+		printf("use ./philo number_of_philosophers time_to_die time_to_eat time_to_sleep "
+			"[number_of_times_each_philosopher_must_eat]");
+	return (0);
 }
